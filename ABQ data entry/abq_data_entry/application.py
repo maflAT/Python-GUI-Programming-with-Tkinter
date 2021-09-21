1cb71f1fa1f985731cdfe199ed9a766e32887ccc
maflAT <13979550+maflAT@users.noreply.github.com>
2021-09-21 18:30:52 +0200

    implemented file selection

--- ABQ data entry/abq_data_entry/application.py
+++ ABQ data entry/abq_data_entry/application.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 from datetime import date
 from .constants import EW
 from . import views as v
@@ -18,7 +18,8 @@ class Application(tk.Tk):
             text="ABQ Data Entry Application",
             font=("TkDefaultFont", 16),
         ).grid(row=0)
-
+        default_filename = f"abc_data_record_{date.today().isoformat()}.csv"
+        self.filename = tk.StringVar(value=default_filename)
         self.settings = {
             "autofill date": tk.BooleanVar(),
             "autofill sheet data": tk.BooleanVar(),
@@ -37,19 +38,28 @@ class Application(tk.Tk):
         self.records_saved = 0
 
     def on_file_select(self):
-        ...
+        """Handle the file->select action from the menu"""
+        filename = filedialog.asksaveasfilename(
+            title="Select the target file for saving records",
+            defaultextension=".csv",
+            filetypes=[("Comma-Separated Values", "*.csv *.CSV")],
+        )
+        if filename:
+            self.filename.set(filename)
 
     def on_save(self):
         # check for errors first
         if e := self.record_form.get_errors():
-            self.status.set(f"Cannot save, error in fields: {', '.join(e.keys())}")
-            message = "Cannot save record"
-            errors = "\n * ".join(e.keys())
-            detail = f"The following fields have errors:\n * {errors}"
-            messagebox.showerror(title="Error", message=message, detail=detail)
+            self.display_errors(e)
             return False
-        filename = f"abc_data_record_{date.today().isoformat()}.csv"
-        m.CSVModel(filename).save_record(self.record_form.get())
+        m.CSVModel(self.filename.get()).save_record(self.record_form.get())
         self.records_saved += 1
         self.status.set(f"{self.records_saved} records saved this session.")
         self.record_form.reset()
+
+    def display_errors(self, e):
+        self.status.set(f"Cannot save, error in fields: {', '.join(e.keys())}")
+        message = "Cannot save record"
+        errors = "\n * ".join(e.keys())
+        detail = f"The following fields have errors:\n * {errors}"
+        messagebox.showerror(title="Error", message=message, detail=detail)
